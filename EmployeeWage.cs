4686d86d75a9f82b232ed8c4b7b5685f3b49623b
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-15 14:03:59 +0530

    Add : Add Part time Employee & Wage - Assume Part time Hour is 8

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -12,16 +12,21 @@ namespace EmpWageCompNonStaticProblem
         {
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
+            int IS_PART_TIME = 2;
 
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
@@ -29,6 +34,7 @@ namespace EmpWageCompNonStaticProblem
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
+
         }
     }
 }
