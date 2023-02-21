5e56b5d679a3a25d2c83579f604c9dd1ddf57bc4
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-21 17:07:21 +0530

    Add : Ability to get the Total Wage when queried by Company.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿namespace EmpWageCompNonStaticProblem
+﻿using EmployeeWageComputationUsingOOPS;
+
+namespace EmpWageCompNonStaticProblem
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            EmployeeWage dMart = new EmployeeWage("DMart", 20, 2, 10);
-            EmployeeWage reliance = new EmployeeWage("Reliance", 10, 4, 20); 
-            dMart.computeEmpWage();
-            Console.WriteLine(dMart.toString());
-            reliance.computeEmpWage();
-            Console.WriteLine(reliance.toString());
+            EmpWageBuiltArray empWageBuiltArray = new EmpWageBuiltArray();
+            empWageBuiltArray.addCompanyEmployeeWage("DMart", 20, 2, 10);
+            empWageBuiltArray.addCompanyEmployeeWage("Reliance", 10, 4, 20);
+            empWageBuiltArray.computeEmpwage();
+            Console.WriteLine("Total wage for DMart company: " + empWageBuiltArray.getTotalwage("DMart"));
         }
     }
 }
